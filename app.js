075952f6f9f0b6331b9643d7229984a2a7c8f042
Sai Kumar Erpina <serpina@uncc.edu>
2020-04-28 01:41:46 -0400

    added code to work with json web token

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/v1/analyzeImage',analyzeImageApi);
+app.use('/api/v1/',analyzeImageApi);
 
 app.use((req, res, next) => {
     const error = new Error("Not found");
--- routes/analyze-image.js
+++ routes/analyze-image.js
@@ -4,6 +4,7 @@ const request = require('request');
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
+const jwt= require('jsonwebtoken');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
@@ -15,22 +16,8 @@ var uriBase = endpoint + 'vision/v2.1/analyze';
 
 //const imageUrl ='https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
 
-
-/**
-* @swagger
-* /api/v1/analyzeImage:
-*   post:
-*     description: analyze an image from the image url
-*     parameters:
- *       - name: imageUrl
- *         in: formData
- *         type: string
- *         required: true     
-*     responses:
-*       '200':
-*         description: OK
-*/
-router.post('/', urlencodedParser, function (req, res) {
+// gets an imageUrl from the body and makes an api request to analyze an image
+const imageAnalyzer = function (req, res) {
   if (req.body) {
     if (req.body.imageUrl) {
       const imageUrl = req.body.imageUrl;
@@ -67,8 +54,85 @@ router.post('/', urlencodedParser, function (req, res) {
   } else {
     return res.send('{}');
   }
+}
+
+//verifying the token obtained from the user.
+const verifyToken = function(req,res,next){
+  var authorization = req.headers['authorization'];
+  if(authorization === null) return res.sendStatus(404);
+  jwt.verify(authorization,'myToken',function(err,req,res){
+      if(err) return res.sendStatus(404);
+      next();
+  })
+
+}
+
+/**
+* @swagger
+* /api/v1/signIn/analyzeImage:
+*   post:
+*     description: generates an token for the user to analyze an image
+*     parameters:
+ *       - name: username
+ *         in: formData
+ *         type: string
+ *         required: true  
+ *       - name: password
+ *         in: formData
+ *         type: string
+ *         required: true   
+*     responses:
+*       '200':
+*         description: OK
+*/
+router.post('/signIn/analyzeImage',function(req,res){
+
+    let username = req.body.username;
+    let password = req.body.password;
+    if(username==="saikumar" && password==="saikumar"){
+        var userData = {
+            user: username,
+    };
+
+    var token = jwt.sign(userData,'myToken',{expiresIn:  "0.2h"})
+    return res.json({accessToken: token});
+}
 });
 
 
+/**
+* @swagger
+* /api/v1/analyzeImage:
+*   post:
+*     description: analyze an image from the image url
+*     parameters:
+ *       - name: imageUrl
+ *         in: formData
+ *         type: string
+ *         required: true     
+*     responses:
+*       '200':
+*         description: OK
+*/
+router.post('/analyzeImage', urlencodedParser, imageAnalyzer)
 
+/**
+* @swagger
+* /api/v1/user/analyzeImage:
+*   post:
+*     description: analyze an image from the image url
+*     parameters:
+ *       - name: imageUrl
+ *         in: formData
+ *         type: string
+ *         required: true   
+ *       - name: Authorization
+ *         in: header
+ *         type: string
+ *         required: true    
+*     responses:
+*       '200':
+*         description: OK
+*/
+router.post('/user/analyzeImage',urlencodedParser,verifyToken, imageAnalyzer )
 module.exports = router;
